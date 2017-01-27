81c55e7376e6a0f1a8321ec936b735760b8f7cf2
Dmitry Sobolev <propp.ds@gmail.com>
2017-01-27 10:18:48 +0500

    Initial commit

---
+++ entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+set -e
+
+if test ! -e /var/run/docker.sock; then
+    echo "Docker unix socket not found!"
+    exit 1
+fi
+
+if test ! -f $CONFIG_FILE; then
+    cat > $CONFIG_FILE <<EOF
+concurrent = $RUNNER_CONCURRENCY
+
+EOF
+fi
+
+exec /usr/bin/dumb-init /entrypoint run --user=$RUNNER_USER --working-directory=$RUNNER_WORK_DIR
